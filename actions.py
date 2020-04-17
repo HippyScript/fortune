259aa81dd41b58dccc8522a47d7003e823f74cdd
HippyScript <63825191+HippyScript@users.noreply.github.com>
2020-04-16 20:28:39 -0400

    Initial upload

---
+++ actions.py
@@ -0,0 +1,63 @@
+import variables
+import sys
+from random import seed
+from random import randint
+
+def buy_items():
+    item = ""
+    print ("╔════════════════════════════════════════╗")
+    print ("║ ░░░░░░░░░░ WHOLESALE PRICES ░░░░░░░░░░ ║")
+    for (k, v) in variables.wholesale.items():
+        print ("║\t\t" + k + ": $" + str(v) + "\t\t ║")
+    print ("╚════════════════════════════════════════╝\n")
+    while item.upper() != "LEAVE":
+        item = input("What item would you like to buy? Type 'leave' to cancel: ")
+        if item.upper() == "LEAVE":
+            return("")
+        quantity = input("How many would you like to buy? ")
+        try:
+            quantity = int(quantity)
+        except ValueError:
+            print ("Please enter a number.")
+        if quantity > 0 and item in variables.wholesale.keys():
+                if quantity * variables.wholesale[item] <= variables.stash["Gold"]:
+                    variables.stash["Gold"] -= quantity * variables.wholesale[item]
+                    variables.stash[item] += quantity
+                    print ("You bought " + str(quantity) + " and have " + str(int(variables.stash["Gold"])) +  " gold remaining.")
+                else:
+                    print ("You don't have enough gold to make that purchase. Too bad you can't sell plasma.")
+
+def rob():
+    seed()
+    success = bool(randint(0, 1))
+
+    if success:
+        variables.stash["Gold"] = variables.stash["Gold"] * 1.3
+        return "Success! You've robbed your competitor and gained gold!"
+    else:
+        variables.stash["Gold"] = variables.stash["Gold"] * .75
+        if variables.stash["Medicine"] > 0:
+            variables.stash["Medicine"] -= 1
+            return """Caught red-handed! You manage to flee, dropping some gold in the process.
+You use one of your medicines to heal the wounds his knife inflicted."""
+        else:
+            sys.exit("""Your competitor caught you! Your blade was quick, but his was quicker.
+As you lay dying on the dusty floor, you have time to ponder such a tragic end.""")
+        
+def advertise():
+    return "NO CODE HERE YET"
+
+def scavenge():
+    return "NO CODE HERE YET"
+
+
+def action_menu(user_choice):
+    if user_choice == 1:
+        return buy_items()
+    elif user_choice == 2:
+        return rob()
+    elif user_choice == 3:
+        return advertise()
+    elif user_choice == 4:
+        return scavenge()
+        
---
+++ fortune.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os, time
+import variables
+import actions
+    
+def update_display(eventStr):
+    print ("╔═══════════════════════════════════════════════════════╗")
+    print ("║ ░░░░░░░░░░░░░░░░░░░░░░░ STATS ░░░░░░░░░░░░░░░░░░░░░░░ ║")
+    print ("║\tStores\t\t\tPrices".ljust(10) + "\t\t\t║")
+    
+    for (k, v), (k2, v2) in zip(variables.stash.items(), variables.prices.items()):
+        print ("║\t" + k + ": " + str(int(v)).ljust(5) + "\t|\t" + "$" + str(v2) + "\t\t\t║")
+        
+    print ("╚═══════════════════════════════════════════════════════╝\n")
+    print (eventStr)
+
+
+
+
+if __name__ == "__main__":
+
+    ti = 0
+    choice = 0
+    while True:
+        time.sleep(2.0)
+        ti += 1
+        if ti % 4 == 0:
+            variables.stash["Gold"] = variables.stash["Gold"] + 1 + variables.indenturedCount
+
+ 
+        
+        if ti % 30 == 0 and variables.indenturedCount > 0:
+            variables.indenturedCount -= 1
+            print ("An indentured servant has paid off his debt and retired.")
+            
+        if ti > 0:
+            update_display(variables.random_event())
+            
+        choice = input("""Buy Supplies [1]\t|\tRob Competitor [2]\t|\tAdvertise [3]\t|\tScavenge [4]\t|\tSleep on It [6]\nWhat would you like to do? """)
+        if int(choice) > 0 and int(choice) <= 6:
+            print (actions.action_menu(int(choice)))
+        else:
+            print (str(choice) + " isn't an option.")
+    
